82c27e9afe227faa60fd7d36412541786bd518b8
Jessica Maple <maplejessbeth@gmail.com>
2020-05-20 17:24:18 -0400

    updates

---
+++ work.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    $('.link').on('click', function(e){
+      
+      var hash = $(this).attr('href');
+       $('html,body').animate({
+         scrollTop: $(hash).offset().top
+       }, 800);
+      
+      return false;
+    });
+  });
